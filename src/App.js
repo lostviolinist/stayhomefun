124027fc324e51eb9845177c172b608f45534dcd
lostviolinist <lostviolinist@gmail.com>
2020-04-17 14:06:49 +0800

    homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 
 import './App.css';
 import Toolbar from './components/Toolbar/Toolbar'
+import HomePage from './components/HomePage/HomePage'
+
 class App extends Component {
   render() {
     return (
+      <Router>
       <header className="nav">
               <Toolbar /> 
       </header>
+      <div className="App container">
+      <Route path="/" component={HomePage} exact />
+          
+      </div>
+      </Router>
     )
   }
 
